58127f24e8a8e3cedac5178704ab1526b5802967
dianahorban <dianahorban477@gmail.com>
2026-05-20 19:17:12 +0200

    The solution

--- task.sql
+++ task.sql
@@ -1,54 +1,57 @@
 --liquibase formatted sql
 
+--changeset mate.acamemy:0 labels:0.0.1
+CREATE DATABASE IF NOT EXISTS ShopDB;
+
 --changeset mate.acamemy:1 labels:0.0.1
-CREATE TABLE Countries (
+CREATE TABLE ShopDB.Countries (
     ID INT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE Countries;
+--rollback DROP TABLE ShopDB.Countries;
 
 --changeset mate.acamemy:2 labels:0.0.1
-CREATE TABLE Products (
+CREATE TABLE ShopDB.Products (
     ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE Products;
+--rollback DROP TABLE ShopDB.Products;
 
 --changeset mate.acamemy:3 labels:0.0.1
-CREATE TABLE Warehouses (
+CREATE TABLE ShopDB.Warehouses (
     ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     Address VARCHAR(50),
     CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (CountryID) REFERENCES ShopDB.Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE Warehouses;
+--rollback DROP TABLE ShopDB.Warehouses;
 
 --changeset mate.acamemy:4 labels:0.0.1
-CREATE TABLE ProductInventory (
+CREATE TABLE ShopDB.ProductInventory (
     ID INT,
     ProductID INT,
-    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (ProductID) REFERENCES ShopDB.Products(ID) ON DELETE NO ACTION,
     WarehouseAmount INT,
     WarehouseID INT,
-    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (WarehouseID) REFERENCES ShopDB.Warehouses(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE ProductInventory;
+--rollback DROP TABLE ShopDB.ProductInventory;
 
 --changeset mate.acamemy:5 labels:0.0.2
-CREATE TABLE Users (
+CREATE TABLE ShopDB.Users (
     ID INT AUTO_INCREMENT,
     FirstName VARCHAR(100),
     LastName VARCHAR(100),
     Email VARCHAR(200),
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE Users;
+--rollback DROP TABLE ShopDB.Users;
 
 --changeset mate.acamemy:6 labels:0.0.3
-CREATE INDEX idx_users_email ON Users (Email);
---rollback DROP INDEX idx_users_email ON Users;
+CREATE INDEX idx_users_email ON ShopDB.Users (Email);
+--rollback DROP INDEX idx_users_email ON ShopDB.Users;
