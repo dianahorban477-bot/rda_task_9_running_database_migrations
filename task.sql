6de826a9b0943f36abd91acb2195ddc2cd5ac82a
dianahorban <dianahorban477@gmail.com>
2026-05-20 19:24:55 +0200

    The solution

--- task.sql
+++ task.sql
@@ -1,57 +1,54 @@
 --liquibase formatted sql
 
---changeset mate.acamemy:0 labels:0.0.1
-CREATE DATABASE IF NOT EXISTS ShopDB;
-
---changeset mate.acamemy:1 labels:0.0.1
-CREATE TABLE ShopDB.Countries (
+--changeset mate.academy:1 labels:0.0.1
+CREATE TABLE Countries (
     ID INT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE ShopDB.Countries;
+--rollback DROP TABLE Countries;
 
---changeset mate.acamemy:2 labels:0.0.1
-CREATE TABLE ShopDB.Products (
+--changeset mate.academy:2 labels:0.0.1
+CREATE TABLE Products (
     ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE ShopDB.Products;
+--rollback DROP TABLE Products;
 
---changeset mate.acamemy:3 labels:0.0.1
-CREATE TABLE ShopDB.Warehouses (
+--changeset mate.academy:3 labels:0.0.1
+CREATE TABLE Warehouses (
     ID INT AUTO_INCREMENT,
     Name VARCHAR(50),
     Address VARCHAR(50),
     CountryID INT,
-    FOREIGN KEY (CountryID) REFERENCES ShopDB.Countries(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE ShopDB.Warehouses;
+--rollback DROP TABLE Warehouses;
 
---changeset mate.acamemy:4 labels:0.0.1
-CREATE TABLE ShopDB.ProductInventory (
+--changeset mate.academy:4 labels:0.0.1
+CREATE TABLE ProductInventory (
     ID INT,
     ProductID INT,
-    FOREIGN KEY (ProductID) REFERENCES ShopDB.Products(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE NO ACTION,
     WarehouseAmount INT,
     WarehouseID INT,
-    FOREIGN KEY (WarehouseID) REFERENCES ShopDB.Warehouses(ID) ON DELETE NO ACTION,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE ShopDB.ProductInventory;
+--rollback DROP TABLE ProductInventory;
 
---changeset mate.acamemy:5 labels:0.0.2
-CREATE TABLE ShopDB.Users (
+--changeset mate.academy:5 labels:0.0.2
+CREATE TABLE Users (
     ID INT AUTO_INCREMENT,
     FirstName VARCHAR(100),
     LastName VARCHAR(100),
     Email VARCHAR(200),
     PRIMARY KEY (ID)
 );
---rollback DROP TABLE ShopDB.Users;
+--rollback DROP TABLE Users;
 
---changeset mate.acamemy:6 labels:0.0.3
-CREATE INDEX idx_users_email ON ShopDB.Users (Email);
---rollback DROP INDEX idx_users_email ON ShopDB.Users;
+--changeset mate.academy:6 labels:0.0.3
+CREATE INDEX idx_users_email ON Users (Email);
+--rollback DROP INDEX idx_users_email ON Users;
